ce50531ddf11655b4ab9679f339f1bd5c6b72d7e
Michel Leblanc <michkele@gmail.com>
2017-11-08 10:45:30 +0200

    Setup production build and server

--- src/tests/components/ExpenseDashBoardPage.test.1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ExpenseDashBoardPage from '../../components/ExpenseDashBoardPage';
-import expenses from '../fixtures/expenses';
-
-test('should render ExpenseDashBoardPage correctly', () => {
-    const wrapper = shallow(<ExpenseDashBoardPage />);
-    expect(wrapper).toMatchSnapshot();
-});
